542e215d8c7f47655eea0f60117edd6731395d87
Oleg Milyukov <milyukov.o@gmail.com>
2025-12-26 15:50:45 +0200

    added dapt to manager

---
+++ ToDoApp.Manager/Controllers/ToDosController.cs
@@ -0,0 +1,15 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApp.Manager.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ToDosController : ControllerBase
+{
+    private readonly DaprClient _daprClient;
+    public ToDosController(DaprClient daprClient)
+    {
+        _daprClient = daprClient;
+    }
+}
