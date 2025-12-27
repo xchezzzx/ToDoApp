9e83d7e3fbfcca373216b924ba280ce14aa2ea41
Oleg Milyukov <milyukov.o@gmail.com>
2025-12-27 15:47:48 +0200

    added endpoint for manager + testing dapr

--- ToDoApp.Manager/Controllers/ToDosController.cs
+++ ToDoApp.Manager/Controllers/ToDosController.cs
@@ -3,13 +3,36 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ToDoApp.Manager.Controllers;
 
-[Route("api/[controller]")]
+[Route("todos")]
 [ApiController]
 public class ToDosController : ControllerBase
 {
     private readonly DaprClient _daprClient;
+
     public ToDosController(DaprClient daprClient)
     {
         _daprClient = daprClient;
+    }
+
+    [HttpGet("{id:Guid}")]
+    public async Task<IActionResult> GetById([FromRoute] Guid id, CancellationToken ct)
+    {
+        const string accessorAppId = "todoapp-accessor";
+
+        try
+        {
+            var todo = await _daprClient.InvokeMethodAsync<object>(
+                HttpMethod.Get,
+                accessorAppId,
+                $"todos/{id}",
+                cancellationToken: ct
+                );
+
+            return Ok(todo);
+        }
+        catch (InvocationException ex) when (ex.Response is not null && (int)ex.Response.StatusCode == 404)
+        {
+            return NotFound();
+        }
     }
 }
