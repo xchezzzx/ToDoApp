60b314c3a9057f5daceb3d5d1a22742e06d64e94
Oleg Milyukov <milyukov.o@gmail.com>
2025-12-31 14:16:11 +0200

    added cors policy

--- ToDoApp.Manager/Program.cs
+++ ToDoApp.Manager/Program.cs
@@ -8,6 +8,17 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDaprClient();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("FrontendDev", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +31,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("FrontendDev");
 
 app.MapControllers();
 
