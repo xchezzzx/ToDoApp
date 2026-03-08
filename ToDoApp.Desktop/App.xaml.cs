1f609396f68c142be211852b201b964b2ef5f8da
Oleg Milyukov <milyukov.o@gmail.com>
2026-03-08 21:39:33 +0200

    Introduce dependency injection in App.xaml.cs
    Refactor App.xaml.cs to use Microsoft.Extensions.DependencyInjection.
    Register HttpClient, ToDoApiClient, MainWindowViewModel, and MainWindow
    as singletons. Remove unused usings and redundant code. This improves
    testability and modularity by enabling dependency injection.

--- ToDoApp.Desktop/App.xaml.cs
+++ ToDoApp.Desktop/App.xaml.cs
@@ -1,14 +1,31 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
 using System.Windows;
+using ToDoApp.Desktop.Services;
+using ToDoApp.Desktop.ViewModels;
 
-namespace ToDoApp.Desktop
+namespace ToDoApp.Desktop;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
-	public partial class App : Application
-	{
-	}
+	public IServiceProvider ServiceProvider { get; private set; } = null!;
 
+	protected override void OnStartup(StartupEventArgs e)
+	{
+		var serviceCollection = new ServiceCollection();
+
+		serviceCollection.AddSingleton(new HttpClient
+		{
+			BaseAddress = new Uri("http://localhost:5084")
+		});
+
+		serviceCollection.AddSingleton<ToDoApiClient>();
+		serviceCollection.AddSingleton<MainWindowViewModel>();
+		serviceCollection.AddSingleton<MainWindow>();
+
+		base.OnStartup(e);
+	}
 }
